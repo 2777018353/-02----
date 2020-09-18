c010f7361cce784f4a8ffad1b5de0a31f1abb5d8
2777018353 <2777018353@qq.com>
2020-09-18 23:32:31 +0800

    银行3

---
+++ 银行04 -- 管理多个储蓄或信用账户/SavingsAccount.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Account.h"
+#include "Accumulator.h"
+class SavingsAccount :
+    public Account
+{
+private:
+    Accumulator acc;
+    double rate;
+
+public:
+    SavingsAccount(Date date, string id, double rate) :acc(date), Account(id), rate(rate) {};
+    
+    double getRate() { return rate; }
+    
+    virtual void record(Date date, double amount, string desc = "interest");
+    virtual void deposit(Date date, double amount, string desc);
+    virtual void withdraw(Date date, double amount, string desc);
+    virtual void settle(const Date& date);
+    // virtual void show();
+};
+
